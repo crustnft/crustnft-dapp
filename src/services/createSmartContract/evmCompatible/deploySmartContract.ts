8c92ec0d41d3a141a78338cdf452268b0a0747aa
nghianguyen119 <trongnghia.nguyen@outlook.com>
2022-02-20 18:24:58 +0100

    Resolve error if wallet change gas price during the transaction

--- src/services/createSmartContract/evmCompatible/deploySmartContract.ts
+++ src/services/createSmartContract/evmCompatible/deploySmartContract.ts
@@ -3,27 +3,28 @@ import { CompilerAbstract } from '@remix-project/remix-solidity';
 import { ContractFactory } from 'ethers';
 import { CONTRACT_NAME } from './constants';
 
-export const deploySmartContract = async (
+export async function deploySmartContract(
   compileResult: CompilerAbstract,
   signer: JsonRpcSigner
-): Promise<TransactionReceipt | undefined> => {
+): Promise<TransactionReceipt | undefined> {
   try {
     const compiledContract = compileResult?.getContract(CONTRACT_NAME);
     const contractBinary = '0x' + compiledContract?.object.evm.bytecode.object;
     const contractABI = compiledContract?.object.abi;
-
     const contractFactory: ContractFactory = new ContractFactory(
       contractABI,
       contractBinary,
       signer
     );
-
     const deployingContract = await contractFactory.deploy();
     const txReceipt = await deployingContract.deployTransaction.wait(1);
-    console.log('success');
-    console.log('transactionReceipt: ', txReceipt);
+    console.log('TransactionReceipt: ', txReceipt);
     return txReceipt;
   } catch (e) {
-    console.log('Error on deploying', e);
+    if (e.reason === 'repriced') {
+      return e.receipt;
+    } else {
+      throw e;
+    }
   }
-};
+}
