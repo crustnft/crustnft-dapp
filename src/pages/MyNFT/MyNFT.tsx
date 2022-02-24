fa7807a59d9edf9c836636daa9ac464a0da4cdde
nghianguyen119 <trongnghia.nguyen@outlook.com>
2022-02-24 15:21:07 +0100

    Remove unused elements

--- src/pages/MyNFT/MyNFT.tsx
+++ src/pages/MyNFT/MyNFT.tsx
@@ -1,37 +1,14 @@
 import { Box, Container, Grid } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import { useState } from 'react';
 import Page from '../../components/Page';
 import AccountBalanceCard from './components/AccountBalanceCard';
 import CallAction from './components/CallAction';
 import MyCollections from './components/MyCollections';
-const TabsWrapperStyle = styled('div')(({ theme }) => ({
-  zIndex: 9,
-  bottom: 0,
-  width: '100%',
-  display: 'flex',
-  position: 'absolute',
-  backgroundColor: theme.palette.background.paper,
-  [theme.breakpoints.up('sm')]: {
-    justifyContent: 'center'
-  },
-  [theme.breakpoints.up('md')]: {
-    justifyContent: 'flex-end',
-    paddingRight: theme.spacing(3)
-  }
-}));
 
 export default function MyNFT() {
-  const [currentTab, setCurrentTab] = useState('nfts');
-
-  const handleChangeTab = (newValue: string) => {
-    setCurrentTab(newValue);
-  };
-
   return (
     <Page title="My NFTs">
       <Container maxWidth="lg">
-        <Grid container>
+        <Grid container spacing={2}>
           <Grid item xs={7}>
             <CallAction />
           </Grid>
@@ -39,7 +16,7 @@ export default function MyNFT() {
             <AccountBalanceCard />
           </Grid>
         </Grid>
-        <Box key="nfts">
+        <Box>
           <MyCollections />
         </Box>
       </Container>
--- src/pages/MyNFT/components/CallAction.tsx
+++ src/pages/MyNFT/components/CallAction.tsx
@@ -3,13 +3,14 @@ import { Button, Card, CardContent, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { Link as RouterLink } from 'react-router-dom';
 import { DocIllustration } from '../../../assets';
-import useLocales from '../../../hooks/useLocales';
 import { PATH_DASHBOARD } from '../../../routes/paths';
 
 const RootStyle = styled(Card)(({ theme }) => ({
   boxShadow: 'none',
   textAlign: 'center',
   backgroundColor: theme.palette.primary.lighter,
+  backgroundImage:
+    'url("https://media.istockphoto.com/vectors/abstract-blur-background-vector-id1077584186")',
   [theme.breakpoints.up('md')]: {
     height: '100%',
     display: 'flex',
@@ -20,8 +21,6 @@ const RootStyle = styled(Card)(({ theme }) => ({
 }));
 
 export default function CallAction() {
-  const { translate } = useLocales();
-
   return (
     <RootStyle>
       <CardContent
@@ -32,11 +31,11 @@ export default function CallAction() {
         }}
       >
         <Typography gutterBottom variant="h4">
-          {translate(`generalApp.welcome`)},
+          Create Now
         </Typography>
 
         <Typography variant="body2" sx={{ pb: { xs: 3, xl: 5 }, maxWidth: 480, mx: 'auto' }}>
-          {translate(`generalApp.slogan`)}
+          Create your own NFTs collection without code.
         </Typography>
 
         <Button
@@ -45,7 +44,7 @@ export default function CallAction() {
           component={RouterLink}
           variant="contained"
         >
-          {translate(`general.learnMore`)}
+          Create Collection
         </Button>
       </CardContent>
 
