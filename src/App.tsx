4f00ab7388259799d4da2ead1ed8d7dd7a6fa2d0
nghianguyen119 <trongnghia.nguyen@outlook.com>
2022-03-07 15:55:22 +0100

    Remove rlt mode, remove loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 // routes
-import { ProgressBarStyle } from './components/LoadingScreen';
 import NotistackProvider from './components/NotistackProvider';
-// components
-import RtlLayout from './components/RtlLayout';
 import ScrollToTop from './components/ScrollToTop';
 import Router from './routes';
 import ThemeProvider from './theme';
@@ -10,13 +7,10 @@ import ThemeProvider from './theme';
 export default function App() {
   return (
     <ThemeProvider>
-      <RtlLayout>
-        <NotistackProvider>
-          <ProgressBarStyle />
-          <ScrollToTop />
-          <Router />
-        </NotistackProvider>
-      </RtlLayout>
+      <NotistackProvider>
+        <ScrollToTop />
+        <Router />
+      </NotistackProvider>
     </ThemeProvider>
   );
 }
