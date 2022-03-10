2f7771ed20a153c8d9ca50f80737b5db44528c12
Huy Dung NGUYEN <huydung.ng.179@gmail.com>
2022-03-10 03:23:49 +0100

    Add buttons

--- src/pages/AssetViewer/components/AssetCard.tsx
+++ src/pages/AssetViewer/components/AssetCard.tsx
@@ -1,35 +1,51 @@
 import FullscreenIcon from '@mui/icons-material/Fullscreen';
-import { Box, Button, Card, Stack } from '@mui/material';
+import { Box, Button, Card, IconButton, Stack, Tooltip } from '@mui/material';
 import LightboxModal from 'components/LightboxModal';
-import { useState } from 'react';
+import { Chain } from 'interfaces/chain';
+import { useEffect, useState } from 'react';
 import { LineScalePulseOutRapid } from 'react-pure-loaders';
+import { getChainByNetworkName } from 'utils/blockchainHandlers';
 import { AssetAndOwnerType } from '../AssetViewer.types';
 
 export default function AssetCard({ assetAndOwner }: { assetAndOwner: AssetAndOwnerType }) {
   const [loading, setLoading] = useState(true);
   const [openLightbox, setOpenLightbox] = useState(false);
+  const [chain, setChain] = useState<Chain | undefined>(undefined);
+
+  useEffect(() => {
+    if (assetAndOwner.chain) {
+      const chainObj = getChainByNetworkName(assetAndOwner.chain);
+      setChain(chainObj);
+    }
+  }, [assetAndOwner]);
 
   const handleOpenLightbox = (url: string) => {
     setOpenLightbox(true);
   };
 
   return (
-    <Card>
-      <Stack spacing={3} sx={{ p: 3, padding: '0px' }}>
-        <Box>
-          <Stack direction="row" alignItems="center" justifyContent="center" sx={{ width: '100%' }}>
-            <Box
-              component="img"
-              alt="post media"
-              src={assetAndOwner.imageUrl}
-              onLoad={() => setLoading(false)}
-              sx={{
-                display: loading ? 'none' : 'block'
-              }}
-            />
-            {loading ? (
-              <></>
-            ) : (
+    <Card sx={{ overflow: 'visible' }}>
+      <Stack sx={{ p: 3, padding: '0px' }}>
+        <Stack
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          sx={{ width: '100%' }}
+        >
+          <Box
+            component="img"
+            alt="post media"
+            src={assetAndOwner.imageUrl}
+            onLoad={() => setLoading(false)}
+            sx={{
+              display: loading ? 'none' : 'block',
+              borderRadius: '15px'
+            }}
+          />
+          {loading ? (
+            <></>
+          ) : (
+            <>
               <Button
                 variant="contained"
                 color="inherit"
@@ -45,25 +61,61 @@ export default function AssetCard({ assetAndOwner }: { assetAndOwner: AssetAndOw
               >
                 View Full
               </Button>
-            )}
-            <LightboxModal
-              images={[assetAndOwner.imageUrl]}
-              mainSrc={assetAndOwner.imageUrl}
-              photoIndex={0}
-              setPhotoIndex={() => {}}
-              isOpen={openLightbox}
-              onCloseRequest={() => setOpenLightbox(false)}
-            />
-          </Stack>
-          <Stack
-            direction="row"
-            alignItems="center"
-            justifyContent="center"
-            sx={{ width: '100%', display: loading ? 'flex' : 'none', mt: 5 }}
-          >
-            <LineScalePulseOutRapid color={'#637381'} loading={loading} />
-          </Stack>
-        </Box>
+              <Box
+                sx={{
+                  textAlign: 'center',
+                  mb: 2,
+                  position: 'absolute',
+                  bottom: '-50px',
+                  borderRadius: '50px',
+                  backgroundColor: 'primary.lighter',
+                  backgroundOpacity: 0.5
+                }}
+              >
+                <Stack direction="row" spacing={2}>
+                  <Tooltip title="Transaction History">
+                    <IconButton
+                      href={`${chain?.blockExplorerUrl || ''}/token/${
+                        assetAndOwner.contractAddress
+                      }?a=${assetAndOwner.tokenId}`}
+                      target="_blank"
+                    >
+                      <Box component="img" src={chain?.icon || ''} sx={{ height: 50 }} />
+                    </IconButton>
+                  </Tooltip>
+                  <Tooltip title="Opensea Viewer">
+                    <IconButton
+                      href={`https://testnets.opensea.io/assets/${assetAndOwner.contractAddress}/${assetAndOwner.tokenId}`}
+                      target="_blank"
+                    >
+                      <Box
+                        component="img"
+                        src="./static/icons/shared/opensea.svg"
+                        sx={{ height: 50 }}
+                      />
+                    </IconButton>
+                  </Tooltip>
+                </Stack>
+              </Box>
+            </>
+          )}
+          <LightboxModal
+            images={[assetAndOwner.imageUrl]}
+            mainSrc={assetAndOwner.imageUrl}
+            photoIndex={0}
+            setPhotoIndex={() => {}}
+            isOpen={openLightbox}
+            onCloseRequest={() => setOpenLightbox(false)}
+          />
+        </Stack>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+          sx={{ width: '100%', display: loading ? 'flex' : 'none', mt: 5 }}
+        >
+          <LineScalePulseOutRapid color={'#637381'} loading={loading} />
+        </Stack>
       </Stack>
     </Card>
   );
