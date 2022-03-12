27621bca54decfe9d40a94af2f61712587564149
nghianguyen119 <trongnghia.nguyen@outlook.com>
2022-03-12 23:29:31 +0100

    Create context

--- src/pages/CPProjectsDashboard/CPProjectsDashboard.tsx
+++ src/pages/CPProjectsDashboard/CPProjectsDashboard.tsx
@@ -1,42 +1,61 @@
 import { Avatar, Container, Grid, Stack, Typography } from '@mui/material';
 import Iconify from 'components/Iconify';
+import useLocalStorage from 'hooks/useLocalStorage';
+import { createContext } from 'react';
 import Page from '../../components/Page';
 import Introduction from './components/Introduction';
 import ProjectCard from './components/ProjectCard';
 import ProjectCardEmpty from './components/ProjectCardEmpty';
 
+type ICPProject = {
+  name: string;
+  description?: string;
+};
+
+const initialLocalStorage: ICPProject[] = [];
+
 export default function CPProjectsDashboard() {
+  const { value: CPProjects, setValueInLocalStorage: setCPProjects } = useLocalStorage(
+    'CPProjects',
+    initialLocalStorage
+  );
+
+  const init = { CPProjects: [], setCPProjects: (newValue: ICPProject[]) => {} };
+  const CPProjectsContext = createContext(init);
+
   return (
     <Page title="Crypto Punks Projects">
       <Container maxWidth="lg">
-        <Introduction />
-        <Stack direction="row" alignItems="center" spacing={2}>
-          <Stack>
-            <Avatar>
-              <Iconify icon="ep:menu" />
-            </Avatar>
-          </Stack>
-          <Stack>
-            <Typography variant="h5" sx={{ color: 'text.primary' }}>
-              PROJECTS
-            </Typography>
+        <CPProjectsContext.Provider value={{ CPProjects, setCPProjects }}>
+          <Introduction />
+          <Stack direction="row" alignItems="center" spacing={2}>
+            <Stack>
+              <Avatar>
+                <Iconify icon="ep:menu" />
+              </Avatar>
+            </Stack>
+            <Stack>
+              <Typography variant="h5" sx={{ color: 'text.primary' }}>
+                PROJECTS
+              </Typography>
 
-            <Typography sx={{ color: 'text.secondary' }}>
-              You have to connect to a wallet to see your projects.
-            </Typography>
+              <Typography sx={{ color: 'text.secondary' }}>
+                You have to connect to a wallet to see your projects.
+              </Typography>
+            </Stack>
           </Stack>
-        </Stack>
-        <Grid container sx={{ pt: 2 }} spacing={2}>
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <ProjectCard />
+          <Grid container sx={{ pt: 2 }} spacing={2}>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <ProjectCard />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <ProjectCardEmpty />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <ProjectCard />
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <ProjectCardEmpty />
-          </Grid>
-        </Grid>
+        </CPProjectsContext.Provider>
       </Container>
     </Page>
   );
