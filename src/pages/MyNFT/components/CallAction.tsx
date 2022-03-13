1a7bf5a53e9f72a94a11bd9e14b9981614d51f8e
nghianguyen119 <trongnghia.nguyen@outlook.com>
2022-03-13 15:59:08 +0100

    Add deer call action

--- src/pages/MyNFT/components/CallAction.tsx
+++ src/pages/MyNFT/components/CallAction.tsx
@@ -1,96 +1,81 @@
-import { Box, Button, Link, Stack, Typography } from '@mui/material';
-// material
+import { Box, Button, Link, Stack, Typography, useTheme } from '@mui/material';
 import { styled } from '@mui/material/styles';
-// import Trianglify from 'trianglify';
-import Trianglify from 'react-trianglify';
+import Iconify from 'components/Iconify';
+import useResponsive from 'hooks/useResponsive';
 
 const WrapStyle = styled(Box)(({ theme }) => ({
-  // backgroundImage: 'url("./static/glassmorphism/trianglify-lowres.png")',
-  backgroundSize: '100% ',
+  backgroundImage: 'url("./static/call-action/deer.jpg")',
+  backgroundPosition: 'center 40%',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
   borderRadius: '15px',
   position: 'relative',
   overflow: 'hidden'
 }));
 
 const RootStyle = styled(Box)(({ theme }) => ({
-  boxShadow: 'none',
   borderRadius: '10px',
   position: 'relative',
-
-  //backgroundColor: theme.palette.primary.lighter,
-
-  // //   backgroundSize: 'cover',
-  // [theme.breakpoints.up('md')]: {
-  //   height: '100%',
-  //   display: 'flex',
-  //   textAlign: 'left',
-  //   alignItems: 'center',
-  //   justifyContent: 'space-between'
-  // }
-  background: 'rgba(255,255,255,0.1)',
   overflow: 'hidden',
-  height: '276px',
-  display: 'flex',
-  textAlign: 'left',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  borderTop: '1px solid rgba(255,255,255,0.5)',
-  borderLeft: '1px solid rgba(255,255,255,0.5)',
-  backdropFilter: 'blur(2px)'
+  height: '300px',
+  display: 'flex'
 }));
 
 export default function CallAction() {
+  const theme = useTheme();
+  const isDesktop = useResponsive('up', 'md');
   return (
     <WrapStyle>
-      <Box sx={{ position: 'absolute', top: '0px', left: '0px' }}>
-        <Trianglify height={276} width={900} cellSize={40} />
-      </Box>
       <RootStyle>
-        <Box
-          component="img"
-          sx={{
-            height: '100%',
-            position: 'absolute',
-            left: { xs: '-50px', sm: '0px' },
-            top: '0px',
-            zIndex: '5'
-          }}
-          src="./static/feature-images/game.png"
-        />
-        <Stack alignItems="flex-end" sx={{ width: '100%', pr: 3, zIndex: '6' }}>
-          <Stack
-            sx={{
-              maxWidth: '250px'
-            }}
-            alignItems={{ xs: 'flex-end', sm: 'flex-start' }}
+        <Stack
+          direction="column"
+          sx={{ width: '100%', height: '100%', pl: { xs: 0, md: 5 }, pb: { xs: 5, md: 0 } }}
+          justifyContent={{ xs: 'flex-end', md: 'center' }}
+          alignItems={{ xs: 'center', md: 'flex-start' }}
+        >
+          <Typography gutterBottom variant="h4" color="#fff" sx={{ pointerEvents: 'none' }}>
+            No Code Required
+          </Typography>
+          <Typography
+            gutterBottom
+            color="#fff"
+            variant="body2"
+            sx={{ pointerEvents: 'none', maxWidth: '300px' }}
+            align={isDesktop ? 'left' : 'center'}
           >
-            <Typography
-              gutterBottom
-              variant="h4"
-              color="rgba(255,255,255,0.4)"
-              sx={{ pointerEvents: 'none' }}
-            >
-              No Code Required
-            </Typography>
+            Create & manage your own NFT collection easily in minutes
+          </Typography>
 
-            <Typography
-              variant="body2"
-              textAlign={{ xs: 'right', sm: 'left' }}
-              sx={{ pb: { xs: 3, xl: 5 }, display: { xs: 'none', sm: 'block' } }}
+          <Stack direction="row" spacing={2}>
+            <Link href="#/create-collection">
+              <Button
+                variant="contained"
+                startIcon={<Iconify icon="bx:plus" />}
+                sx={[
+                  {
+                    backgroundColor: '#3772FF',
+                    color: '#ffffff',
+                    borderRadius: '32px'
+                  },
+                  { '& .MuiButton-startIcon': { mr: 0.5 } }
+                ]}
+              >
+                Create
+              </Button>
+            </Link>
+            <Button
+              variant="outlined"
+              sx={[
+                {
+                  color: '#fff',
+                  borderRadius: '32px',
+                  border: theme.palette.header.walletButtonBorder,
+                  borderColor: '#fff'
+                }
+              ]}
             >
-              Create your own NFTs collection in minutes
-            </Typography>
-
-            <Stack direction="row">
-              <Link href="#/create-collection">
-                <Button
-                  variant="contained"
-                  sx={{ borderRadius: '20px', boxShadow: '0 5px 15px rgba(0,0,0,0.5)' }}
-                >
-                  Create Collection
-                </Button>
-              </Link>
-            </Stack>
+              My Wallet
+            </Button>
           </Stack>
         </Stack>
       </RootStyle>
