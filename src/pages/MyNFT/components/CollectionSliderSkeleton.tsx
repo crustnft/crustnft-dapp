34165d25beb89a2b1fde3f37350f51bd8660bb96
ttran <ttran@iorga.com>
2022-03-17 01:10:14 +0100

    update CollectionSliderSkeleton

--- src/pages/MyNFT/components/CollectionSliderSkeleton.tsx
+++ src/pages/MyNFT/components/CollectionSliderSkeleton.tsx
@@ -1,21 +1,78 @@
-import { Box, Divider, Skeleton, Stack } from '@mui/material';
-import NFTCardSkeleton from './NFTCardSkeleton';
+import { Box, Card, CardHeader, Paper, Skeleton, Stack } from '@mui/material';
+import Slider from 'react-slick';
 
 export default function CollectionSliderSkeleton() {
+  const settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  };
+
   return (
-    <Box>
-      <Box sx={{ backgroundColor: '#F4F6F8', p: 2 }}>
-        <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
-          <Stack direction="column" sx={{ width: '200px' }}>
-            <Skeleton animation="wave" height={15} width="50%" style={{ marginBottom: 6 }} />
-            <Skeleton animation="wave" height={15} width="100%" />
-          </Stack>
-        </Stack>
-      </Box>
-      <Divider />
-      <Box sx={{ pt: 2, pl: 5 }}>
-        <NFTCardSkeleton />
-      </Box>
-    </Box>
+    <Card sx={{ p: { xs: 0.5, sm: 0.75, md: 1 } }}>
+      <CardHeader
+        title={<Skeleton animation="wave" height={30} width="100%" />}
+        sx={{
+          pl: 2,
+          pr: 5,
+          mb: 1
+        }}
+      />
+      <Stack sx={{ m: 1 }}>
+        <Slider {...settings}>
+          <Box sx={{ px: 1, py: 0, position: 'relative' }}>
+            <Paper
+              sx={{
+                boxShadow: 'none',
+                borderRadius: '18px',
+                width: '100%',
+                aspectRatio: '1 / 1.25'
+              }}
+            >
+              <Stack
+                alignItems="center"
+                justifyContent="center"
+                direction="column"
+                spacing={1}
+                sx={{ height: '100%' }}
+              >
+                <Skeleton
+                  variant="rectangular"
+                  width="100%"
+                  sx={{ height: 'inherit', p: 2, borderRadius: '18px' }}
+                ></Skeleton>
+              </Stack>
+            </Paper>
+          </Box>
+
+          {[...Array(3)].map((_, index) => (
+            <Box key={index} />
+          ))}
+        </Slider>
+      </Stack>
+    </Card>
   );
 }
