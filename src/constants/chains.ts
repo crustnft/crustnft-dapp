8af08c5afc9b3a9a606f812b1be444fb469b9854
ttran <ttran@iorga.com>
2022-03-23 21:50:42 +0100

    add chain url dictionary

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -33,7 +33,7 @@ const TEST_CHAINS = [
     name: 'Rinkeby',
     currencySymbol: 'RIN',
     icon: './static/icons/networks/rinkeby.svg',
-    iconDark: './static/icons/networks/rinkeby-dark.png',
+    iconDark: './static/icons/networks/rinkeby-dark.svg',
     chainId: 4,
     rpcUrl: 'https://rinkeby.infura.io/v3/741c5f1257a24106934fe4105c784478',
     blockExplorerUrl: 'https://rinkeby.etherscan.io'
@@ -45,7 +45,7 @@ const TEST_CHAINS = [
     iconDark: './static/icons/networks/binance-dark.png',
     chainId: 97,
     rpcUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
-    blockExplorerUrl: 'https://testnet.bscscan.com/'
+    blockExplorerUrl: 'https://testnet.bscscan.com'
   }
 ];
 
