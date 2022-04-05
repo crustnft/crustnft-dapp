aae8278fdfd5cb5f24480099a36449d9c4f952fb
nghianguyen119 <trongnghia.nguyen@outlook.com>
2022-04-06 01:09:48 +0200

    Avoid future error

--- src/constants/openseaChainUrl.ts
+++ src/constants/openseaChainUrl.ts
@@ -1,11 +1,9 @@
-const openseaUrlDictionary = new Map<string, string>([
-  ['Binance', ''],
-  ['Polygon', 'https://opensea.io/assets/matic'],
-  ['Ethereum', 'https://opensea.io/assets'],
-  ['Klaytn', 'https://opensea.io/assets/klaytn'],
-  ['BSC Testnet', ''],
-  ['Rinkeby', 'https://testnets.opensea.io/assets'],
-  ['Mumbai', 'https://testnets.opensea.io/assets/mumbai']
+export const openseaUrlDictionary = new Map<string, string>([
+  ['binance', ''],
+  ['polygon', 'https://opensea.io/assets/matic'],
+  ['ethereum', 'https://opensea.io/assets'],
+  ['klaytn', 'https://opensea.io/assets/klaytn'],
+  ['bsc testnet', ''],
+  ['rinkeby', 'https://testnets.opensea.io/assets'],
+  ['mumbai', 'https://testnets.opensea.io/assets/mumbai']
 ]);
-
-export default openseaUrlDictionary;
