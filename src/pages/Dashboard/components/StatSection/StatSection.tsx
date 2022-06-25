33a52f528da81553f0759e9a9440ea3a05822a7c
Huy Dung NGUYEN <huydung.ng.179@gmail.com>
2022-06-25 14:01:32 +0200

    collection revenue

--- src/pages/Dashboard/components/StatSection/StatSection.tsx
+++ src/pages/Dashboard/components/StatSection/StatSection.tsx
@@ -1,6 +1,6 @@
 import { Stack } from '@mui/material';
-import { COLLECTION_STAT } from 'pages/Dashboard/constants';
-import { StatStatusBody } from './StatSectionBody';
+import { COLLECTION_REVENUE, COLLECTION_STAT } from 'pages/Dashboard/constants';
+import { StatRevenueBody, StatStatusBody } from './StatSectionBody';
 import StatSectionHeader from './StatSectionHeader';
 
 const StatSection = () => {
@@ -8,6 +8,8 @@ const StatSection = () => {
     <Stack>
       <StatSectionHeader {...COLLECTION_STAT} />
       <StatStatusBody />
+      <StatSectionHeader {...COLLECTION_REVENUE} />
+      <StatRevenueBody />
     </Stack>
   );
 };
--- src/pages/Dashboard/components/StatSection/StatSectionBody/StatRevenueBody.tsx
+++ src/pages/Dashboard/components/StatSection/StatSectionBody/StatRevenueBody.tsx
@@ -1,5 +1,104 @@
+import { Box, Stack, Typography } from '@mui/material';
+import { useState } from 'react';
+
+interface CollectionBtnProps {
+  title: string;
+  total: number;
+  isSelected: boolean;
+  onClick: () => void;
+}
+
+const COLLECTIONS = ['Testnet collection', 'Mainnet collection'];
+const REVENUE_INFO = [
+  {
+    title: 'Mint revenue',
+    total: '5 ETH'
+  },
+  {
+    title: 'Trade revenue',
+    total: '3 ETH'
+  },
+  {
+    title: 'Total revenue',
+    total: '8 ETH'
+  }
+];
+
+const CollectionBtn = ({ title, total, isSelected, onClick }: CollectionBtnProps) => {
+  return (
+    <Box
+      sx={[
+        {
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          border: '2px solid',
+          borderRadius: '12px',
+          cursor: 'pointer',
+          gap: '6px',
+          padding: '10px 25px'
+        },
+        isSelected
+          ? {
+              borderColor: 'accent.main',
+              backgroundColor: 'accent.lighter'
+            }
+          : {
+              borderColor: 'grey.300'
+            }
+      ]}
+      onClick={onClick}
+    >
+      <Typography variant="subtitle2" color="grey.900">
+        {title}
+      </Typography>
+      <Typography variant="h6" color="accent.dark" sx={{ display: 'flex', alignItems: 'baseline' }}>
+        {total}
+        <Typography variant="subtitle2" color="accent.dark" sx={{ ml: '3px' }}>
+          collections
+        </Typography>
+      </Typography>
+    </Box>
+  );
+};
+
+const RevenueElement = ({ title, total }: { title: string; total: string }) => {
+  return (
+    <Box sx={{ flex: 1, height: '100%', borderRadius: '12px', backgroundColor: 'grey.100' }}>
+      <Stack sx={{ alignItems: 'center', p: '55px 35px', gap: '10px' }}>
+        <Typography variant="subtitle1" color="grey.700">
+          {title}
+        </Typography>
+        <Typography variant="h6" color="secondary">
+          {total}
+        </Typography>
+      </Stack>
+    </Box>
+  );
+};
+
 const StatRevenueBody = () => {
-  return <div>StatRevenueBody</div>;
+  const [selected, setSelected] = useState(COLLECTIONS[0]);
+  return (
+    <Stack sx={{ flexDirection: 'row', gap: '5%' }}>
+      <Stack sx={{ flex: 1 }}>
+        <Stack sx={{ gap: '15px' }}>
+          {COLLECTIONS.map((collection, index) => (
+            <CollectionBtn
+              key={index}
+              title={collection}
+              total={5}
+              isSelected={selected === collection}
+              onClick={() => setSelected(collection)}
+            />
+          ))}
+        </Stack>
+      </Stack>
+      {REVENUE_INFO.map((info, index) => (
+        <RevenueElement key={index} title={info.title} total={info.total} />
+      ))}
+    </Stack>
+  );
 };
 
 export default StatRevenueBody;
