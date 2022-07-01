3bfad7edc16ff6fbefaa10f7c12e78c3f1ad1fa4
Huy Dung NGUYEN <huydung.ng.179@gmail.com>
2022-07-02 00:48:01 +0200

    change style intro create collection

--- src/pages/IntroCreateCollection/IntroCreateCollection.tsx
+++ src/pages/IntroCreateCollection/IntroCreateCollection.tsx
@@ -1,4 +1,5 @@
-import { Button, Card, Container, Grid, Link, Stack, Typography } from '@mui/material';
+import { Card, Container, Grid, Link, Stack, Typography } from '@mui/material';
+import MCard from 'components/@material-extend/MCard';
 import image1 from '../../assets/images/create10K.png';
 import image2 from '../../assets/images/createMultiple.png';
 import Page from '../../components/Page';
@@ -11,63 +12,63 @@ export default function LearnMore() {
         <Introduction />
         <Grid container spacing={3} justifyContent="center">
           <Grid item xs={12} sm={6} md={3}>
-            <Card sx={{ backgroundColor: 'background.neutral', p: 2 }}>
-              <Card
-                sx={{
-                  backgroundColor: 'background.paper',
-                  aspectRatio: 'auto'
-                }}
+            <Link href="#/my-collections" underline="none">
+              <MCard
+                hoverEffect
+                sx={{ backgroundColor: 'background.neutral', p: 2, height: '100%' }}
               >
-                <img
-                  src={image1}
-                  alt="create 10k collection"
-                  style={{ width: '100%', objectFit: 'fill' }}
-                />
-              </Card>
-              <Stack alignItems="center" sx={{ pt: 2 }} spacing={2}>
-                <Link href="#/my-collections" underline="none">
-                  <Button
-                    variant="text"
-                    sx={{ borderRadius: '8px', color: 'text.primary', fontSize: '1.125rem' }}
-                  >
+                <Card
+                  sx={{
+                    backgroundColor: 'background.paper',
+                    aspectRatio: 'auto'
+                  }}
+                >
+                  <img
+                    src={image1}
+                    alt="create 10k collection"
+                    style={{ width: '100%', objectFit: 'fill' }}
+                  />
+                </Card>
+                <Stack alignItems="center" sx={{ pt: 2 }} spacing={2}>
+                  <Typography variant="h6" color="text.primary">
                     Create 10K collection
-                  </Button>
-                </Link>
-                <Typography variant="body1" color="text.secondary" align="center">
-                  Upload different pieces of your collection and let us generate a complete
-                  collection
-                </Typography>
-              </Stack>
-            </Card>
+                  </Typography>
+                  <Typography variant="body1" color="text.tertiary" align="center">
+                    Upload different pieces of your collection and let us generate a complete
+                    collection
+                  </Typography>
+                </Stack>
+              </MCard>
+            </Link>
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
-            <Card sx={{ backgroundColor: 'background.neutral', p: 2 }}>
-              <Card
-                sx={{
-                  backgroundColor: 'background.paper',
-                  aspectRatio: 'auto'
-                }}
+            <Link href="#/mint-exp-nft" underline="none">
+              <MCard
+                hoverEffect
+                sx={{ backgroundColor: 'background.neutral', p: 2, height: '100%' }}
               >
-                <img
-                  src={image2}
-                  alt="create multiple collection"
-                  style={{ width: '100%', objectFit: 'fill' }}
-                />
-              </Card>
-              <Stack alignItems="center" sx={{ pt: 2 }} spacing={2}>
-                <Link href="#/create-expandable-collection" underline="none">
-                  <Button
-                    variant="text"
-                    sx={{ borderRadius: '8px', color: 'text.primary', fontSize: '1.125rem' }}
-                  >
+                <Card
+                  sx={{
+                    backgroundColor: 'background.paper',
+                    aspectRatio: 'auto'
+                  }}
+                >
+                  <img
+                    src={image2}
+                    alt="create multiple collection"
+                    style={{ width: '100%', objectFit: 'fill' }}
+                  />
+                </Card>
+                <Stack alignItems="center" sx={{ pt: 2 }} spacing={2}>
+                  <Typography variant="h6" color="text.primary">
                     Create Multiple
-                  </Button>
-                </Link>
-                <Typography variant="body1" color="text.secondary" align="center">
-                  Create a collection that can be expanded to include more arts at any time
-                </Typography>
-              </Stack>
-            </Card>
+                  </Typography>
+                  <Typography variant="body1" color="text.tertiary" align="center">
+                    Create a collection that can be expanded to include more arts at any time
+                  </Typography>
+                </Stack>
+              </MCard>
+            </Link>
           </Grid>
         </Grid>
       </Container>
